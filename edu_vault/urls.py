b3ea801539c7c87d5d5584296db0bca92b4e712c
kondwani kamundi <kkamu@MacBook-Pro.local>
2024-12-12 22:14:34 +0200

    added grade book and problem bank models

--- edu_vault/urls.py
+++ edu_vault/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("lti/", include("lti_provider.urls")),
+    path("lti/", include("lti_provider.urls")),
 ]
