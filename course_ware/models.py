76289082dc21a8bb436b11558ad70e4cd16730b6
kkamu <kkamundi@gmail.com>
2025-05-10 04:31:18 +0200

    refactor: move user model from course ware ref #111
    - moved user model from course ware into users django app

--- course_ware/models.py
+++ course_ware/models.py
@@ -42,22 +42,7 @@ CLASS_CHOICES = [
 ATTEMPT_STATUS_CHOICES = [(mode.value, mode.name) for mode in AttemptStatusEnum]
 
 
-class EdxUser(models.Model):
-    """Holds Edx user information. Not the Django primary user Model"""
 
-    id = models.PositiveIntegerField(
-        primary_key=True, unique=True, help_text="edX user ID"
-    )
-    username = models.CharField(max_length=255, unique=True, help_text="edX username")
-    email = models.EmailField(blank=True, help_text="edX email")
-    active = models.BooleanField(default=True)
-
-    class Meta:
-        verbose_name = "edX User"
-        verbose_name_plural = "edX Users"
-
-    def __str__(self):
-        return self.username
 
 
 class AcademicClass(models.Model):
--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class EdxUser(models.Model):
+    """Holds Edx user information. Not the Django primary user Model"""
+
+    id = models.PositiveIntegerField(
+        primary_key=True, unique=True, help_text="edX user ID"
+    )
+    username = models.CharField(max_length=255, unique=True, help_text="edX username")
+    email = models.EmailField(blank=True, help_text="edX email")
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "edX User"
+        verbose_name_plural = "edX Users"
+
+    def __str__(self):
+        return self.username
