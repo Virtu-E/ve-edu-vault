f13a0b22462ad3e0073f51defc987daf118750b7
kkamu <kkamundi@gmail.com>
2025-05-16 00:40:57 +0200

    refactor: rename module from services to lib
    - Since we want to remove all business logic from our views, this logic will go into special services.py files in each django module. Hence, made sense to rename the original services module to lib

--- src/services/course_sync/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CourseSyncConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "course_sync"
-
-    def ready(self):
-        pass
